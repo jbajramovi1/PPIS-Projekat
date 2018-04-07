985757ec890780c936a53b289260f50be463ded7
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-04-07 23:49:05 +0200

    Initial project commit

---
+++ netflixmgmt/src/main/java/com/example/netflixmgmt/NetflixmgmtApplication.java
@@ -0,0 +1,12 @@
+package com.example.netflixmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixmgmtApplication.class, args);
+	}
+}
