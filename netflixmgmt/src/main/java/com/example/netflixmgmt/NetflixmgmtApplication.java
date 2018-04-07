2e090319ab7c868e4f012d5bce320c2471379e8b
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-04-08 00:21:40 +0200

    develop-create initial packages

--- netflixmgmt/src/main/java/com/example/netflixmgmt/NetflixmgmtApplication.java
+++ netflixmgmt/src/main/java/com/example/netflixmgmt/NetflixmgmtApplication.java
@@ -3,6 +3,7 @@ package com.example.netflixmgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class NetflixmgmtApplication {
 
