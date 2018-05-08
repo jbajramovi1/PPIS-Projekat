3bf14cf1e3f1f3a3545054eddb9f064fc9adffda
Jasmina Bajramovic <jbajramovi1@etf.unsa.ba>
2018-05-08 12:19:40 +0200

    Register change request form

--- netflixmgmt-fe/src/App.js
+++ netflixmgmt-fe/src/App.js
@@ -15,7 +15,7 @@ import './scss/style.css'
 // Containers
 import { DefaultLayout } from './containers';
 // Pages
-import { Login, Page404, Page500, Register } from './views/Pages';
+import { Login, Page404, Page500, Register, RegisterCR } from './views/Pages';
 
 // import { renderRoutes } from 'react-router-config';
 
@@ -28,6 +28,7 @@ class App extends Component {
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
+          <Route exact path="/registercr" name="Register Change Request" component={RegisterCR} />
           <Route path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
