c1aca99f7d9621ba309ecdf6f3a6559eadcb8c2a
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-05-09 13:52:52 +0200

    Jasmina's route fixed to position the form inside the default layout

--- netflixmgmt-fe/src/App.js
+++ netflixmgmt-fe/src/App.js
@@ -28,7 +28,6 @@ class App extends Component {
           <Route exact path="/register" name="Register Page" component={Register} />
           <Route exact path="/404" name="Page 404" component={Page404} />
           <Route exact path="/500" name="Page 500" component={Page500} />
-          <Route exact path="/registercr" name="Register Change Request" component={RegisterCR} />
           <Route path="/" name="Home" component={DefaultLayout} />
         </Switch>
       </HashRouter>
