4b2cb4710ce2882619b9fd42cb365c99238a405e
Ajša Terko <aterko1@etf.unsa.ba>
2018-05-15 19:13:05 +0200

    Change Request Show By ID

--- netflixmgmt-fe/src/routes.js
+++ netflixmgmt-fe/src/routes.js
@@ -2,13 +2,15 @@ import {
   Dashboard
 } from './views';
 import { RegisterCR } from './views/Pages'; 
+import { ViewCR } from './views/Pages'; 
 import DefaultLayout from './containers/DefaultLayout';
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/registercr', name: 'RegisterChangeRequest', component: RegisterCR}
+  { path: '/registercr', name: 'RegisterChangeRequest', component: RegisterCR},
+  { path: '/viewcr/:id', name: 'ViewChangeRequest', component: ViewCR}
 ];
 
 export default routes;
