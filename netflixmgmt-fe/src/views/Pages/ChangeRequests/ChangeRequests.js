99a91c34d705ead7dc865e5593b5918412ae8117
Anisa Hadzibulic <ahadzibuli1@etf.unsa.ba>
2018-05-17 15:39:18 +0200

    failure

--- netflixmgmt-fe/src/views/Pages/ChangeRequests/ChangeRequests.js
+++ netflixmgmt-fe/src/views/Pages/ChangeRequests/ChangeRequests.js
@@ -10,52 +10,36 @@ const API_ROUTE = 'http://localhost:8080/changeRequest/all';
 
 class ChangeRequests extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-
-        }
-        ;
-
-
-    }
-
-    fetchStats() {
-        axios.get(API_ROUTE, {
-            headers: {'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbmlzYSIsImV4cCI6MTUyNzQxNzMxOH0.n4IlskGFqyl-cNpRUhp3nzTGeXMd6rgJPJ_HZNSEMlrCEciKLcwJA9InhrCza5xmnfSzAxLa7RPEzzd7ammAew'}}
-        ).then((response) => {
-            let pieData = response.data;
-            let newPie = this.state.pie;
-            let keys = Object.keys(pieData);
-            let i = 1;
-            let len = keys.length;
-            let prope;
-            let value;
-            while (i<len){
-                prope = keys[i];
-                value = pieData[prope];
-                newPie.labels.push(prope);
-                newPie.datasets[0].data.push(value);
-                i = i+ 1;
-            }
-
-            this.setState({
-                pie:newPie
-            })
-            console.log(this.state.pie.labels)
-        }).catch(error => {
-
-        });
-    }
-
-    componentDidMount() {
-
-    }
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //     requests:[]
+    //     }
+    //     ;
+    //
+    //
+    // }
+    //
+    //
+    // componentDidMount() {
+    //     axios.get('http://localhost:8080/changeRequest/all',{})
+    //         .then( response => {
+    //             this.setState({requests: response.data});
+    //         })
+    // }
 
     render() {
-        return (
+        // let changeRequests = Array.from(this.state.requests).map((request) =>
+        //     <option value={request.name}>{request.description}</option>
+        // );
 
+        return (
             <div className="app flex-row align-items-center">
+                <Container>
+                    <Row>
+                        <h1>Change Requests</h1>
+                    </Row>
+                </Container>
 
             </div>
         );
--- netflixmgmt-fe/src/views/index.js
+++ netflixmgmt-fe/src/views/index.js
@@ -23,7 +23,7 @@ import Charts from './Charts';
 import Dashboard from './Dashboard';
 import { Flags, FontAwesome, SimpleLineIcons } from './Icons';
 import { Alerts, Badges, Modals } from './Notifications';
-import { Login, Page404, Page500, Register, ChangeRequests } from './Pages';
+import { Login, Page404, Page500, Register} from './Pages';
 import { Colors, Typography } from './Theme';
 import Widgets from './Widgets';
 
